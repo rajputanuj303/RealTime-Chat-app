05964f3b46546a1677a894863c07563753723332
rajputanuj303 <rajputanuj303@gmail.com>
2026-02-07 00:46:20 +0530

    Done

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -51,7 +51,7 @@ const Sidebar = ({ selectedUser, setSelectedUser }) => {
             className={`relative flex items-center gap-2 p-2 pl-4 rounded cursor-pointer max-sm:text-sm ${selectedUser?._id === user._id && "bg-[#282142]/50"}`}
           >
             <img
-              src={user ? user.profilePic : assets.avatar_icon}
+              src={user?.profilePic || assets.avatar_icon}
               alt=""
               className="w-[35px] aspect-[1/1] rounded-full"
             />
@@ -63,7 +63,11 @@ const Sidebar = ({ selectedUser, setSelectedUser }) => {
                 <span className="text-neutral-400 text-xs">Offline</span>
               )}
             </div>
-            {index > 2 && <p>{index}</p>}
+            {index > 2 && (
+              <p className="absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50">
+                {index}
+              </p>
+            )}
           </div>
         ))}
       </div>
